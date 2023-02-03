e24afaf76708085b02992e17c16049b1c26d10ce
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-02 22:51:43 -0500

    Updated file import

--- Data Import.R
+++ Data Import.R
@@ -1,1 +1,13 @@
-### Data Import
+### Data Import ###
+install.packages('pacman')
+library(pacman)
+p_load(tidyverse)
+
+eligibility <- read_csv("Raw Data/eligibility.csv", 
+                        col_names = c("Date", "Percent of Poverty Line"), skip = 2)
+forego <- read_csv("Raw Data/foregoing-care.csv", 
+                   col_names = c("Abv", "Location", "Year", "Format", "Percent", "MOE"), skip = 6)
+ervisits <- read_csv("Raw Data/er-visits.csv", 
+                     col_names = c("Date", "ER Visits Per Thousand"), skip = 2)
+finances <- read_csv("Raw Data/finances.csv", 
+                     skip = 4)
