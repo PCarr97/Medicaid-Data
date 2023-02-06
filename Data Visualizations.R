b5a2c69cf33fb3d81014c312ef23fb4602c91131
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-06 16:28:30 -0500

    Added titles; fixed hstatus ggplot code

--- Data Visualizations.R
+++ Data Visualizations.R
@@ -17,18 +17,19 @@ eligibility |>
 # Forego plot
 forego |> ggplot() +
   geom_line(aes(x = Year, y = Percent), size = 1.5, color = "darkgreen") +
+  geom_vline(aes(xintercept = as.Date(Year[9])), linetype = 4, size = 1.75, color = "red") +
   ylim(0, 0.17) +
   xlab("Year") +
   ylab("% Foregoing") +
-  ggtitle("Percentage Foregoing Medical Care Due to Cost") +
-  geom_vline(aes(xintercept = as.Date(Year[9])), linetype = 4, size = 1.75, color = "red")
+  ggtitle("Percentage Foregoing Medical Care Due to Cost")
+
 
 
 
 # ER Visits plot
 ervisits |> ggplot() +
   geom_line(aes(x = Year, y = Visits), size = 1.5, color = 'red2') +
-  ylim(0, 450) +
+  ylim(200, 450) +
   xlab("Year") + 
   ylab("Number of ER Visits") +
   ggtitle("Emergency Room Visits per Thousand Americans")
@@ -39,6 +40,7 @@ ervisits |> ggplot() +
 finances |> ggplot(aes(x = Year)) +
   geom_point(aes(y = Debt, color = 'red'), size = 2.5) +
   geom_point(aes(y = Assets, color = "blue"), size = 2.5) +
+  geom_vline(aes(xintercept = as.Date(Year[5])), linetype = 4, size = 1.75, color = "darkgreen") +
   xlab("Year") + 
   ylab("Amount in Thousands") +
   ggtitle("Debts and Assets of Lowest 20% of American Earners") +
@@ -49,8 +51,27 @@ finances |> ggplot(aes(x = Year)) +
 
 
 
+#Health Status of Male v Female example plot
+hstatus |> ggplot(aes(x = Year)) + 
+  geom_line(aes(y = White, color = 'blue4'), size = 1.5) +
+  geom_line(aes(y = Black, color = 'darkgreen'), size = 1.5) +
+  scale_color_identity(name = '',
+                       breaks = c('blue4', 'darkgreen'),
+                       labels = c("White", "Black"),
+                       guide = 'legend')
 
-# Eligibility vs. Percentage foregoing care #
+
+
+#Health status of those under poverty line
+hstatus |> ggplot(aes(x = Year)) + 
+  geom_line(aes(y = Under100pct, color = 'red'), size = 1.5) +
+  geom_vline(aes(xintercept = as.Date(Year[9]), color = "blue"), linetype = 4, size = 1.75)
+
+
+
+
+#############################################################################################################
+# Example of combining two data sets #
 ggplot() +
   geom_point(data = eligibility, aes(x = Year, y = Percent), size = 2.5, color = "blue") +
   geom_line(data = forego, aes(x = Year, y = Percent), size = 1.5) +
