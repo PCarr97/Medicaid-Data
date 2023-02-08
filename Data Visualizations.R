9c8ce72492e2fe15d66ad1f18cd053f5191cf0bc
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-07 20:35:48 -0500

    Finalized color palettes

--- Data Visualizations.R
+++ Data Visualizations.R
@@ -6,14 +6,14 @@ p_load(tidyverse, dplyr, ggplot2)
 # Eligibility plot
 eligibility |> 
   ggplot() +
-  geom_point(aes(x = Year, y = Percent), size = 2.5, color = "blue") +
+  geom_point(aes(x = Year, y = Percent), size = 2.5, color = "#1d65a0") +
   ylim(0, 1.4) +
   xlab("Year") + 
   ylab("Eligibility as % of Poverty Line") +
   ggtitle("Medicaid Eligibility Trend") +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+  theme(panel.background = element_rect(fill = '#fcf9f7', color = 'black'),
+        panel.grid.major = element_line(color = '#818a92', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = '#818a92', linetype = 'dotted'),
         axis.text.x = element_text(color = 'black', size = 11),
         axis.text.y = element_text(color = 'black', size = 11),
         axis.title.x = element_text(color = 'black', size = 12.5),
@@ -24,15 +24,15 @@ eligibility |>
 
 # Forego plot
 forego |> ggplot() +
-  geom_line(aes(x = Year, y = Percent), size = 1.5, color = "darkgreen") +
-  geom_vline(aes(xintercept = as.Date(Year[9])), linetype = 4, size = 1.75, color = "red") +
+  geom_line(aes(x = Year, y = Percent), size = 1.5, color = "#f19633") +
+  geom_vline(aes(xintercept = as.Date(Year[9])), linetype = 'twodash', size = 1.5, color = "#1d65a0") +
   ylim(0, 0.17) +
   xlab("Year") +
   ylab("% Foregoing") +
   ggtitle("Percentage Foregoing Medical Care Due to Cost") +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+  theme(panel.background = element_rect(fill = '#fcf9f7', color = 'black'),
+        panel.grid.major = element_line(color = '#818a92', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = '#818a92', linetype = 'dotted'),
         axis.text.x = element_text(color = 'black', size = 11),
         axis.text.y = element_text(color = 'black', size = 11),
         axis.title.x = element_text(color = 'black', size = 12.5),
@@ -44,14 +44,15 @@ forego |> ggplot() +
 
 # ER Visits plot
 ervisits |> ggplot() +
-  geom_line(aes(x = Year, y = Visits), size = 1.5, color = 'red2') +
+  geom_line(aes(x = Year, y = Visits), size = 1.5, color = '#7d0540') +
+  geom_vline(aes(xintercept = as.Date(Year[9])), linetype = 'twodash', size = 1.5, color = "#1d65a0") +
   ylim(200, 450) +
   xlab("Year") + 
   ylab("Number of ER Visits") +
   ggtitle("Emergency Room Visits per Thousand Americans") +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+  theme(panel.background = element_rect(fill = '#fcf9f7', color = 'black'),
+        panel.grid.major = element_line(color = '#818a92', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = '#818a92', linetype = 'dotted'),
         axis.text.x = element_text(color = 'black', size = 11),
         axis.text.y = element_text(color = 'black', size = 11),
         axis.title.x = element_text(color = 'black', size = 12.5),
@@ -63,38 +64,19 @@ ervisits |> ggplot() +
 
 #Finances plot
 finances |> ggplot(aes(x = Year)) +
-  geom_point(aes(y = Debt, color = 'red'), size = 2.5) +
-  geom_point(aes(y = Assets, color = "blue"), size = 2.5) +
-  geom_vline(aes(xintercept = as.Date(Year[5])), linetype = 4, size = 1.75, color = "darkgreen") +
+  geom_point(aes(y = Debt, color = '#fc5c88'), size = 2.75) +
+  geom_point(aes(y = Assets, color = "black"), size = 2.75) +
+  geom_vline(aes(xintercept = as.Date(Year[5])), linetype = 'twodash', size = 1.5, color = "#1d65a0") +
   xlab("Year") + 
   ylab("Amount in Thousands") +
   ggtitle("Debts and Assets of Lowest 20% of American Earners") +
   scale_color_identity(name = '',
-                       breaks = c('blue', 'red'),
+                       breaks = c('black', '#fc5c88'),
                        labels = c("Assets", "Debt"),
                        guide = 'legend') +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
-        axis.text.x = element_text(color = 'black', size = 11),
-        axis.text.y = element_text(color = 'black', size = 11),
-        axis.title.x = element_text(color = 'black', size = 12.5),
-        axis.title.y = element_text(color = 'black', size = 12.5),
-        plot.title = element_text(size = 14, face = "bold"))
-
-
-
-#Health Status of White v Black example plot
-hstatus |> ggplot(aes(x = Year)) + 
-  geom_line(aes(y = White, color = 'blue4'), size = 1.5) +
-  geom_line(aes(y = Black, color = 'darkgreen'), size = 1.5) +
-  scale_color_identity(name = '',
-                       breaks = c('blue4', 'darkgreen'),
-                       labels = c("White", "Black"),
-                       guide = 'legend') +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+  theme(panel.background = element_rect(fill = '#fcf9f7', color = 'black'),
+        panel.grid.major = element_line(color = '#818a92', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = '#818a92', linetype = 'dotted'),
         axis.text.x = element_text(color = 'black', size = 11),
         axis.text.y = element_text(color = 'black', size = 11),
         axis.title.x = element_text(color = 'black', size = 12.5),
@@ -105,16 +87,16 @@ hstatus |> ggplot(aes(x = Year)) +
 
 #Health status of those under poverty line
 hstatus |> ggplot(aes(x = Year)) + 
-  geom_line(aes(y = Under100pct, color = 'red'), size = 1.5) +
-  geom_vline(aes(xintercept = as.Date(Year[9]), color = "blue"), linetype = 4, size = 1.75) +
-  #ylim(0,22) +
+  geom_line(aes(y = Under100pct), color = '#00853f', size = 1.5) +
+  geom_vline(aes(xintercept = as.Date(Year[9])), color = "#1d65a0", linetype = 'twodash', size = 1.5) +
+  ylim(18,22) +
   xlab("Year") + 
   ylab("Percentage") +
   ggtitle("Percentage of Impoverished Reporting Fair or Poor Health") + 
   theme(legend.position="none") +
-  theme(panel.background = element_rect(fill = 'white', color = 'black'),
-        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
-        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+  theme(panel.background = element_rect(fill = '#fcf9f7', color = 'black'),
+        panel.grid.major = element_line(color = '#818a92', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = '#818a92', linetype = 'dotted'),
         axis.text.x = element_text(color = 'black', size = 11),
         axis.text.y = element_text(color = 'black', size = 11),
         axis.title.x = element_text(color = 'black', size = 12.5),
@@ -134,6 +116,24 @@ ggplot() +
   ylab("Eligibility as % of Poverty Line") +
   ggtitle("Medicaid Eligibility Trends")
 
+
+
+#Health Status of White v Black example plot
+hstatus |> ggplot(aes(x = Year)) + 
+  geom_line(aes(y = White, color = 'blue4'), size = 1.5) +
+  geom_line(aes(y = Black, color = 'darkgreen'), size = 1.5) +
+  scale_color_identity(name = '',
+                       breaks = c('blue4', 'darkgreen'),
+                       labels = c("White", "Black"),
+                       guide = 'legend') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major = element_line(color = 'black', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = 'black', linetype = 'dotted'),
+        axis.text.x = element_text(color = 'black', size = 11),
+        axis.text.y = element_text(color = 'black', size = 11),
+        axis.title.x = element_text(color = 'black', size = 12.5),
+        axis.title.y = element_text(color = 'black', size = 12.5),
+        plot.title = element_text(size = 14, face = "bold"))
 
 
 
